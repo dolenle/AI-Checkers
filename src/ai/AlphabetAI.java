5d125072aef95193bb9bf55b5c3e7a5b3fdbf16a
dolenle <le2@cooper.edu>
2015-10-29 22:01:06 -0400

    AlphabetAI time limit enforcement

--- src/ai/AlphabetAI.java
+++ src/ai/AlphabetAI.java
@@ -3,6 +3,7 @@ package ai;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
 
 import checkersGame.Board;
 import checkersGame.Move;
@@ -14,6 +15,8 @@ public class AlphabetAI implements Player {
 	private int playerTeam;
 	private Scanner input = new Scanner(System.in);
 	private Random rand = new Random();
+	
+	private long stopTime;
 				
 	public AlphabetAI(int team) {
 		playerTeam = team;
@@ -35,57 +38,53 @@ public class AlphabetAI implements Player {
 	}
 	
 	public Move selectMove(ArrayList<Move> validMoves, Board b) {
-		int depth = 7;
+		int depth = 1;
 		Move bestMove = validMoves.get(0);
 		Move lastBest = bestMove;
+		int best = Integer.MIN_VALUE;
 		if(validMoves.size() == 1) {
 			return bestMove;
 		}
-		long start = System.nanoTime();
-		long lastTime = 0;
-		long now = start;
-		while(now - start < timeLimit) {
-			lastTime = now - start;
-			int best = Integer.MIN_VALUE;
+		
+		long startTime = System.nanoTime();
+		stopTime = startTime + timeLimit;
+		
+		while(best != Integer.MAX_VALUE) {
+			best = Integer.MIN_VALUE;
 			lastBest = bestMove;
+			long lastTime = System.nanoTime();
+			
 			int alpha = Integer.MIN_VALUE;
 			int beta = Integer.MAX_VALUE;
+			
 			for(Move m : validMoves) {
-				int score = search(depth, m, b, playerTeam, alpha, beta);
-				if(score > best || score == best && rand.nextBoolean()) {
-					alpha = best = score;
-					bestMove = m;
-				}
-				if(beta < best) {
-					break;
+				try {
+					int score = search(depth, m, b, playerTeam, alpha, beta);
+					if(score > best || score == best && rand.nextBoolean()) {
+						alpha = best = score;
+						bestMove = m;
+					}
+					if(beta < best) {
+						break;
+					}
+				}  catch (TimeoutException te) {
+					System.out.println("Search time limit reached. Reverting to depth "+--depth);
+					System.out.println("Reached depth "+depth+" in "+(lastTime - startTime)/1000000000.0+"s");
+					return lastBest;
 				}
 			}
-//			int best2 = best;
-//			best = Integer.MIN_VALUE;
-//			for(Move m : validMoves) {
-//				int score = regularSearch(depth, m, b, playerTeam);
-//				if(score > best || score == best && rand.nextBoolean()) {
-//					best = score;
-//					bestMove = m;
-//				}
-//			}
-//			if(best2 != best) {
-//				System.out.println("AlphaBeta score mismatch!");
-//				System.exit(1);
-//			}
 			
-			now = System.nanoTime();
 			depth++;
 		}
-		if(now - start > timeLimit) {
-			System.out.println("Time limit exceeded! Reverting to depth "+--depth);
-			bestMove = lastBest;
-		}
-		System.out.println("Reached depth "+depth+" in "+(lastTime)/1000000000.0+"s");
+
+		System.out.println("Reached depth "+(depth-1)+" in "+(System.nanoTime() - startTime)/1000000000.0+"s");
 		return bestMove;
 	}
 	
-	private int search(int depth, Move m, Board b, int team, int alpha, int beta) {
+	private int search(int depth, Move m, Board b, int team, int alpha, int beta) throws TimeoutException {
+		if(System.nanoTime() > stopTime) {
+			throw new TimeoutException();
+		}
 		if(depth == 0) {
 			int score = evaluate(m, b.applyMove(m));
 			b.undoMove(m);
@@ -103,57 +102,17 @@ public class AlphabetAI implements Player {
 				return Integer.MAX_VALUE;
 			}
 			for(Move next : branches) {
-				int score = search(depth-1, next, b, -team, alpha, beta);
-				int score2 = regularSearch(depth-1, next, b, -team);
-				if(score > value) {
-					alpha = value = score;
-				}
-				if(beta < value) {
-					break;
-				}
-			}
-		} else {
-			value = Integer.MAX_VALUE;
-			if(branches.size() == 0) {
-				b.undoMove(m);
-				return Integer.MIN_VALUE;
-			}
-			for(Move next : branches) {
-				int score = search(depth-1, next, b, -team, alpha, beta);
-				int score2 = regularSearch(depth-1, next, b, -team);
-				if(score < value) {
-					beta = value = score;
-				}
-				if(value < alpha) {
-					break;
-				}
-			}
-		}
-		b.undoMove(m);
-		return value;
-	}
-	
-	private int regularSearch(int depth, Move m, Board b, int team) {
-		if(depth == 0) {
-			int score = evaluate(m, b.applyMove(m));
-			b.undoMove(m);
-			return score;
-		}
-
-		b.applyMove(m);
-		ArrayList<Move> branches = b.getValidMovesSingleThread(-team);
-				
-		int value;
-		if(team == playerTeam) {
-			value = Integer.MIN_VALUE;
-			if(branches.size() == 0) {
-				b.undoMove(m);
-				return Integer.MAX_VALUE;
-			}
-			for(Move next : branches) {
-				int score = regularSearch(depth-1, next, b, -team);
-				if(score > value) {
-					value = score;
+				try {
+					int score = search(depth-1, next, b, -team, alpha, beta);
+					if(score > value) {
+						alpha = value = score;
+					}
+					if(beta < value) {
+						break;
+					}
+				} catch (TimeoutException te) {
+					b.undoMove(m);
+					throw te;
 				}
 			}
 		} else {
@@ -163,9 +122,17 @@ public class AlphabetAI implements Player {
 				return Integer.MIN_VALUE;
 			}
 			for(Move next : branches) {
-				int score = regularSearch(depth-1, next, b, -team);
-				if(score < value) {
-					value = score;
+				try {
+					int score = search(depth-1, next, b, -team, alpha, beta);
+					if(score < value) {
+						beta = value = score;
+					}
+					if(value < alpha) {
+						break;
+					}
+				} catch (TimeoutException te) {
+					b.undoMove(m);
+					throw te;
 				}
 			}
 		}
@@ -175,11 +142,27 @@ public class AlphabetAI implements Player {
 	
 	//heuristic
 	public int evaluate(Move m, Board b) {
+		int redScore, blackScore, score;
+		redScore = 4*(b.getRedPieces().size());
+		redScore += 2*b.getKingCount(Piece.RED);
+		blackScore = 4*(b.getBlackPieces().size());
+		blackScore += 2*b.getKingCount(Piece.BLACK);
+		
 		if(playerTeam == Piece.RED) {
-			return b.getRedPieces().size()-b.getBlackPieces().size();
+			if(blackScore == 0) {
+				return Integer.MAX_VALUE;
+			}
+			score = (redScore*1024)/blackScore;
 		} else {
-			return b.getBlackPieces().size()-b.getRedPieces().size();
-	}
+			if(redScore == 0) {
+				return Integer.MAX_VALUE;
+			}
+			score = (blackScore*1024)/redScore;
+		}
+		if(m.isPromotion()) {
+			score += 1000;
+		}
+		return score;
 	}
 	
 	public int getTeam() {
